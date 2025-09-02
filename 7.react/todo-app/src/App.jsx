fa2ac09ca9ab2bb53f0ad5ec3f3444cc9336f4c8
fatimaBenazzou <82899317+fatimaBenazzou@users.noreply.github.com>
2025-09-02 10:34:14 +0100

    configuration

---
+++ 7.react/todo-app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div>
+            <span className="icon-[iwwa--delete] w-8 h-8 text-amber-300"></span>
+        </div>
+    );
+}
