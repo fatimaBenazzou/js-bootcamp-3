79d9b86d3e1f3aa6412bf5bd39fb36f82415d41b
fatimaBenazzou <82899317+fatimaBenazzou@users.noreply.github.com>
2025-09-16 14:34:39 +0100

    ts

---
+++ 8.ts/test-ts/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nameL = "Fatima";
+console.log(`Hello ! My name is ${nameL} and i'm your instructor ^^`);
+let age = 24;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ 8.ts/test-ts/src/index.ts
@@ -0,0 +1,6 @@
+let nameL: string = "Fatima";
+
+console.log(`Hello ! My name is ${nameL} and i'm your instructor ^^`);
+
+let age: number = 24;
+console.log(age);
